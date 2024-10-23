357980061f4566b58ec9e412de5d1e0ecd3f46fb
icwmahin <icwmahin@gmail.com>
2024-10-24 04:03:12 +0600

    little work

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,7 @@
-document.getElementById("openSidebar").addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.add("show");
+document.getElementById("openSidebar").addEventListener("click", function () {
+  document.querySelector(".sidebar").classList.add("show");
 });
 
-document.getElementById("closeSidebar").addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.remove("show");
+document.getElementById("closeSidebar").addEventListener("click", function () {
+  document.querySelector(".sidebar").classList.remove("show");
 });
-
-// dark and light mode toggle button
